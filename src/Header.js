e6735cb11e6bf2843f49ca5b4cc02804be686efa
Y <techprivacy@gmail.com>
2023-09-05 13:09:10 +0530

    myreact

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+  return (
+   <>
+   <ul className='xyz'>
+<li>Home</li>
+<li>About</li>
+<li>Contact</li>
+   </ul>
+<a href='#'>Hello</a>   
+   </>
+  )
+}
